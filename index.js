3c0a46bf454d14c0fe26ba162130e43e5da2793e
Jayanth R <r.jayanth@outlook.com>
2022-12-08 15:41:08 +0530

    feat: send connection successful, total users list, new user detail to socket client

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
-
-//New imports
-const http = require('http').Server(app);
-const cors = require('cors');
-
-app.use(cors());
-
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+const httpServer = require('http').createServer();
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://192.168.0.105:3000',
+  },
 });
 
-//Add this before the app.get() block
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
+io.use((socket, next) => {
+  const userDetails = socket.handshake.auth.userDetails;
+  // console.log('in use handler ', userDetails);
+  // if (userDetails.userName) {
+  //   return next(new Error("invalid userName"));
+  // }
+  socket.userDetails = { ...userDetails, id: socket.id };
+  next();
+});
+
+io.on('connection', (socket) => {
+  console.log('Active sockets ', io.engine.clientsCount);
+  console.log('user connected ', socket.id, socket.userDetails.userName);
+
+  socket.emit('connection successful', socket.userDetails);
+
+  // fetch existing users
+  let users = [];
+  for (let [id, socket] of io.of('/').sockets) {
+    users.push(socket.userDetails);
+  }
+
+  // send total users list to the newly connected user
+  console.log('usersList ', users);
+  socket.emit('users list', users);
+
+  // notify existing users about new user
+  socket.broadcast.emit('new user', socket.userDetails);
+
+  // forward the private message to the right recipient
+  socket.on('private message', ({ content, to }) => {
+    socket.to(to).emit('private message', {
+      content,
+      from: socket.id,
     });
-});
+  });
 
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'Hello world',
+  // notify users upon disconnection
+  socket.on('disconnect', () => {
+    console.log('user disconnected ', socket.id);
+    console.log('Active sockets ', io.engine.clientsCount);
+    socket.broadcast.emit(
+      'user disconnected',
+      socket.id,
+      socket.userDetails.userName
+    );
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+const PORT = process.env.PORT || 4000;
+
+httpServer.listen(PORT, () =>
+  console.log(`server listening at http://localhost:${PORT}`)
+);
